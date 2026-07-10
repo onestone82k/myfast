2082d0c0635355bb0a6aee8e4167811b81a8396a
Hanseok Lim <hslim@pivotbridge.com>
2026-07-10 14:01:41 +0900

    API 호출 데모

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# CORS 설정
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
     return {"msg":"Hello World!"}
+
+@app.get("/api/message")
+def get_message():
+  return {"message":"안녕하세요. 백엔드 메세지 호출하셨죠?"}
