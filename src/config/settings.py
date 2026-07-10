418a0a1fb01cf41e24648a12842e7114e32b0130
Hanseok Lim <hslim@pivotbridge.com>
2026-07-10 17:55:35 +0900

    db명 수정

--- src/config/settings.py
+++ src/config/settings.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
   APP_NAME: str = "FastAPI App"
-  DATABASE_URL: str = "mysql+pymysql://pivotdev:djfudnsdkagh4$@52.79.138.161:3306/pivot"
+  DATABASE_URL: str = "mysql+pymysql://pivotdev:djfudnsdkagh4$@pivot-mysql:3306/pivot"
   JWT_SECRET: str = "super-secret-key"
 
   class Config:
