30464948d4517a91dcd99cff1220cd35c6dd0a13
Hanseok Lim <hslim@pivotbridge.com>
2026-07-16 19:39:59 +0900

    new

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,3 +1,4 @@
+import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 # 1. OS 환경변수에서 'APP_ENV' 값을 읽어온다. (없으면 기본값: local)
@@ -5,15 +6,15 @@ env_state = os.getenv("APP_ENV", "local")
 
 class Settings(BaseSettings):
   APP_NAME: str = "FastAPI App"
-  DATABASE_URL: str
-  # DATABASE_URL: str = "mysql+pymysql://pivotdev:djfudnsdkagh4$@mysql:3306/pivot"  
+  DATABASE_URL: str  
   JWT_SECRET: str
   
   # 2. env_state 값에 따라 읽어들일 파일을 동적으로 결정
   model_config = SettingsConfigDict(
     env_file=f".env.{env_state}",
-    env_file_encoding="utf-8"
+    env_file_encoding="utf-8",
+    extra="ignore"
   )
- 
+
 settings = Settings()
 
