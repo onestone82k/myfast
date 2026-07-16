3576bea83d62fd2e0228140aa4547e8d9e3576b5
Hanseok Lim <hslim@pivotbridge.com>
2026-07-16 18:47:40 +0900

    self-hosted로 수정

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,14 @@ FROM python:3.11-slim
 
 WORKDIR /app
 
+# 시스템 의존성 설치 (MySQL 연결 등을 위해 필요할수있음)
+RUN apt-get update && apt-get install -y gcc default-libmysqlclient-dev && rm -rf /var/lib/apt/lists/*
+
 # requirements.txt 복사 및 설치
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
-# 현재 폴더(myfast)의 모든 소스코드를 컨테이너 내부로 복사
+# 현재 폴더(backend)의 모든 소스코드를 컨테이너 내부로 복사
 COPY . .
 
 CMD ["uvicorn", "src.main:app", "--host", "0.0.0.0", "--port", "8000"]
--- src/config/settings.py
+++ src/config/settings.py
@@ -1,13 +1,19 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+# 1. OS 환경변수에서 'APP_ENV' 값을 읽어온다. (없으면 기본값: local)
+env_state = os.getenv("APP_ENV", "local")
 
 class Settings(BaseSettings):
   APP_NAME: str = "FastAPI App"
-  DATABASE_URL: str = "mysql+pymysql://pivotdev:djfudnsdkagh4$@mysql:3306/pivot"
-  # DATABASE_URL: str = "mysql+pymysql://pivotdev:djfudnsdkagh4$@52.79.138.161:3306/pivot"
-  JWT_SECRET: str = "super-secret-key"
-
-  class Config:
-    env_file = ".env"   # .env 파일이 있으면 우선 로드
-
+  DATABASE_URL: str
+  # DATABASE_URL: str = "mysql+pymysql://pivotdev:djfudnsdkagh4$@mysql:3306/pivot"  
+  JWT_SECRET: str
+  
+  # 2. env_state 값에 따라 읽어들일 파일을 동적으로 결정
+  model_config = SettingsConfigDict(
+    env_file=f".env.{env_state}",
+    env_file_encoding="utf-8"
+  )
+ 
 settings = Settings()
 
